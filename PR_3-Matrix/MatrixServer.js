629a950131c0e10a5101f9e570d53c135ff78133
itsmohdzaheer <zaheershaik0323@gmail.com>
2026-01-29 07:11:31 +0530

    origin main

--- PR_3-Matrix/MatrixServer.js
+++ PR_3-Matrix/MatrixServer.js
@@ -4,9 +4,26 @@ const PORT = 8080;
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 
-app.get('/', (req, res)=>{
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/tables", (req, res) => {
+  res.render("tables");
+});
+
+app.get("/widgets", (req, res) => {
+  res.render("widgets");
+});
+
+app.get("/grid", (req, res) => {
+  res.render("grid");
+});
+
+app.get("/charts", (req, res) => {
+  res.render("charts");
+});
+
 app.listen(PORT, ()=>{
     console.log(`Server started at port http://localhost:${PORT}`)
 })
