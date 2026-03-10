104fd099ac6f613769a6c48dc272045a6e183c9e
itsmohdzaheer <zaheershaik0323@gmail.com>
2026-03-10 10:12:44 +0530

    Unlisted The PR_8

--- PR_8-Catogories/model/catogory.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = mongoose.Schema({
-    category: String,
-    categoryImage: String
-});
-
-module.exports = mongoose.model("Category", categorySchema);
