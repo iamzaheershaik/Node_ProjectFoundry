2b82b50abba6a6feebeb59f5c87e6acfebaca8d1
iamzaheershaik <zaheershaik0323@gmail.com>
2026-04-09 18:56:59 +0530

    express complete kar

---
+++ JavaScript_Projects/Node_Js_Projects/Project_1/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const taskRoute = require("./routes/tasks.routes");
+const connectDB = require('./db');
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+    res.send("Hello World")
+}) 
+
+app.use("/api/v1/tasks", taskRoute)
+
+const start = async () => {
+    try{
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is Running on http://localhost:${PORT}`);
+        })
+    }catch (err){
+        console.error(err);
+    }
+}
+start()
--- Web-Server/03-simple-protocol/protocol.js
+++ Web-Server/03-simple-protocol/protocol.js
@@ -1,5 +1,11 @@
 class MessageReader {
     constructor(){
-        this.buffer = this.buffer.alloc()
+        this.buffer = this.buffer.alloc(0);
+        this.messages = [];
+    }
+
+    push(data){
+        this.buffer = this.buffer.concat([this.buffer, data])
+        while(this.buffer.concat([this.buffer, data]))
     }
 }
